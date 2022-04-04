b6b1f2bbd9b98effb7249e0409545d78b278504b
quantwoori <sangil.bae@wooriam.kr>
2022-04-04 15:17:41 +0900

    _

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ Starting Pair Trade
 
     step 1) Find pairs
         - Correlation test.
-        - Historical Correlation test.
+            -> Historical return correlation to determine pairs
+            -> Note: Use "PRICE SPREAD"
     step 2) Find Hedge Ratios
         - OLS, TLS, Johansen Cointegration Test etc.
         - For this case we will use TLS
@@ -16,11 +17,11 @@ Starting Pair Trade
     
 """
 
-# Get Pairs
+# Step 1) Get Pairs
 p = ProvePairs()
 approve = p.test_pairs()
 
-# Get Spreads
+# Step 2) Find Hedge Ratios
 for stock_pairs in approve:
     s1, s2 = stock_pairs
     spd = Spread(s1, s2)
